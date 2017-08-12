d5df610730f00f37114591772e6ecd77eddbbd10
Tobias Kronthaler <kronthto@gmail.com>
2017-08-12 18:50:16 +0200

    Adjusted 404 site (text from old page, home link, Helmet)

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { Helmet } from 'react-helmet'
+import { Link } from 'react-router-dom'
 
 class NoMatch extends Component {
   shouldComponentUpdate() {
@@ -18,7 +20,20 @@ class NoMatch extends Component {
   }
 
   render() {
-    return <div>Sorry, page not found</div>
+    return (
+      <div>
+        <Helmet>
+          <title>Not found</title>
+        </Helmet>
+        <h1 className="title">Not found</h1>
+        <strong>We can't find that page you're looking for</strong>
+        <p>
+          Maybe you mistyped something, maybe the page isn't there anymore,
+          maybe it did never exist and maybe it will in the future
+        </p>
+        <Link to={'/'} title="Home page">Go to home page</Link>
+      </div>
+    )
   }
 }
 
