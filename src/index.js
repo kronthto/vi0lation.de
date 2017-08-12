2c42a0322e44d0cef249f45df65aaccd46b9fe49
Tobias Kronthaler <kronthto@gmail.com>
2017-08-12 18:39:12 +0200

    Style: Added custom Bulma build

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
 import configureStore from './store'
+import './style/bulma.css'
 import './index.css'
 import App from './containers/App'
 import registerServiceWorker from './registerServiceWorker'
