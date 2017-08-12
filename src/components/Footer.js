0d3f5e8f59d19e592f4c954e29ebc72535482388
Tobias Kronthaler <kronthto@gmail.com>
2017-08-12 20:51:38 +0200

    Basic layout

---
+++ src/components/Footer.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+class Footer extends Component {
+  shouldComponentUpdate() {
+    return false
+  }
+
+  render() {
+    return (
+      <footer role="contentinfo" className="footer">
+        <div className="container has-text-right">
+          <Link to="/contact">Contact</Link>
+          <img
+            src="//cdn.vi0lation.de/img/viologo.png"
+            width="36"
+            height="18"
+            alt="Vi0 Logo"
+            title="Vi0 Logo"
+            style={{ marginLeft: '20px' }}
+          />
+        </div>
+      </footer>
+    )
+  }
+}
+
+export default Footer
--- src/containers/App.js
+++ src/containers/App.js
@@ -4,18 +4,38 @@ import { Helmet } from 'react-helmet'
 import { renderRoutes } from 'react-router-config'
 import routes from '../routes'
 
+import Navi from '../components/Navi'
+import Footer from '../components/Footer'
+
+const defaultMeta = () => {
+  return (
+    <Helmet defaultTitle="Vi0" titleTemplate="%s - Vi0">
+      <meta
+        name="description"
+        content="Vi0lation brigade website and AirRivals ranking"
+      />
+      <link rel="dns-prefetch" href="https://api.vi0lation.de" />
+    </Helmet>
+  )
+}
+
 class App extends Component {
   render() {
     return (
       <div>
-        <Helmet defaultTitle="Cool app" titleTemplate="%s - Cool app">
-          <meta
-            name="description"
-            content="This is the default meta description"
-          />
-        </Helmet>
+        {defaultMeta()}
 
-        {renderRoutes(routes)}
+        <header role="banner">
+          <Navi />
+        </header>
+
+        <main className="section">
+          <div className="container content">
+            {renderRoutes(routes)}
+          </div>
+        </main>
+
+        <Footer />
       </div>
     )
   }
