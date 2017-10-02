0aced9b536cb6a89e6333293d674c811bd006739
Tobias Kronthaler <kronthto@gmail.com>
2017-10-02 18:54:01 +0200

    React v16: Use hydrate instead of render

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import registerServiceWorker from './registerServiceWorker'
 const initialState = window.DATA ? window.DATA : {}
 const store = configureStore(initialState)
 
-ReactDOM.render(
+ReactDOM.hydrate(
   <Provider store={store}>
     <BrowserRouter>
       <ScrollToTop>
