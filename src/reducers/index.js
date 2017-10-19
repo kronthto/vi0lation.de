c1f260099ef821cf309c8fd380ac8129d2ac8114
Tobias Kronthaler <kronthto@gmail.com>
2017-10-19 19:15:18 +0200

    Include highscores reducer, remove boilerplate

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux'
 
-import posts from './posts'
-import comments from './comments'
+import highscores from './highscores'
 
 export default combineReducers({
-  posts,
-  comments
+  highscores
 })
