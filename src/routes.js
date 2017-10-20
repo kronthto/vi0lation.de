15aa633ff2f0309edf9d201fbabda711941808ac
Tobias Kronthaler <kronthto@gmail.com>
2017-10-20 17:41:04 +0200

    Added Highscores Route Redirect to last date + ToDo

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,6 @@
+import React from 'react'
+import { Redirect } from 'react-router'
+
 import Home from './components/Home'
 import TS3List from './components/TS3List'
 import TS404Server from './components/TS404Server'
@@ -7,6 +10,10 @@ import Contact from './components/Contact'
 import EPList from './components/AR/EPList'
 
 import NoMatch from './components/NoMatch'
+
+import { maxDate } from './data/dataset'
+
+export const highscoresUrl = '/ranking/de/highscores'
 
 const routes = [
   {
@@ -30,9 +37,15 @@ const routes = [
     path: '/contact',
     component: Contact
   },
+  // TODO: Ranking Info page: Methodik, Datenbestand, Motivation, etc.
   {
     path: '/ranking/eplist',
     component: EPList
+  },
+  {
+    path: highscoresUrl,
+    exact: true,
+    component: () => <Redirect to={highscoresUrl + '/' + maxDate} />
   },
   {
     component: NoMatch
