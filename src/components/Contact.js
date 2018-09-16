e580ebb8b5009b230741277d1298ea70a35ba840
Tobias Kronthaler <kronthto@gmail.com>
2018-09-16 13:50:42 +0200

    techguy_t Pic update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Helmet } from 'react-helmet'
 
-import tobiasImg from '../img/techguy_t.png'
+import tobiasImg from '../img/techguy_t.jpg'
 
 const SrcLinkFE = () => {
   return (
