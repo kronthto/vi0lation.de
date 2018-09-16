93a7605c19ce4976a267a70d28c4809d8d730510
Tobias Kronthaler <kronthto@gmail.com>
2018-09-16 14:04:10 +0200

    FB Tool Meta

--- src/components/AR/FBTool.js
+++ src/components/AR/FBTool.js
@@ -21,7 +21,26 @@ class FBTool extends Component {
 
         <h1 className="title">FrameBreak Tool</h1>
 
+        <section className="section">
+          Standard weapons in AceOnline / AirRivals / SCO (Space Cowboy Online)
+          only profit from very specific values of enchanted reattack (so called
+          "breakpoints") due to time quantization.<br />
+          The following tool can be used to calculate the optimal re-attack
+          enchants to use those breakpoints:
+        </section>
+
         <FrameBreakCalculator />
+
+        <p style={{ fontSize: '80%' }}>
+          <a
+            href="https://github.com/kronthto/ao-framebreak"
+            target="_blank"
+            title="AceOnline framebreak calculator Source-Code"
+            rel="noopener noreferrer"
+          >
+            source on GitHub
+          </a>
+        </p>
       </div>
     )
   }
