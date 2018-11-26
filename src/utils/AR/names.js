0eb50dcab408816a67f75a4c647522f0f5b3d46c
Tobias Kronthaler <kronthto@gmail.com>
2018-11-26 19:54:50 +0100

    LUD4K special color Kappa

--- src/utils/AR/names.js
+++ src/utils/AR/names.js
@@ -5,14 +5,29 @@ const colorMap = {
   c: '#00FFFF'
 }
 
+function specialNames(name) {
+  switch (name) {
+    case 'LUD4K':
+      return (
+        <span>
+          LUD<i style={{ color: 'red' }}>4K</i>
+        </span>
+      )
+    default:
+      return name
+  }
+}
+
 export function colorName(name) {
   let regex = name.match(/^\\([a-z])(.*)\\[a-z]$/)
   if (!regex) {
-    return name
+    return specialNames(name)
   }
   let color = colorMap[regex[1]]
   if (!color) {
-    return name
+    return specialNames(name)
   }
-  return <span style={{ color }}>{regex[2]}</span>
+  name = specialNames(regex[2])
+
+  return <span style={{ color }}>{name}</span>
 }
