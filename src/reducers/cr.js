c3cfa5b09d26a805feb00c320245a5e236f4347f
Tobias Kronthaler <kronthto@gmail.com>
2019-10-04 12:42:16 +0200

    :zap: Limit CR dates dropdown to 7 days back with all dates, fix duplicate 23:55 and 00:00

--- src/reducers/cr.js
+++ src/reducers/cr.js
@@ -2,6 +2,7 @@ import { REQUESTDATES, RECEIVEDATES } from '../types/cr'
 import { getCurrentTS } from '../utils/api'
 import dayDiff from 'date-fns/difference_in_days'
 import format from 'date-fns/format'
+import sub_days from 'date-fns/sub_days'
 
 const initialState = {
   dates: null
@@ -22,6 +23,8 @@ export default function reducer(state = initialState, action) {
       let days = []
 
       const midNightRegex = /^00:0[0-5]$/
+      const twentyThreeHoursRegex = /^23:\d\d$/
+      let nextDay // Skip for this day if 23:55
 
       return Object.assign({}, state, {
         dates: Object.assign({}, state.dates, {
@@ -31,18 +34,23 @@ export default function reducer(state = initialState, action) {
               return true
             }
             let dt = new Date(date)
-            if (dayDiff(now, dt) <= 10) {
+            let dayDiffDt = dayDiff(now, dt)
+            if (dayDiffDt <= 6) {
               return true
             }
             let dayFrmt = format(dt, 'YYYY-MM-DD')
+            let dateHm = format(dt, 'HH:mm')
             if (days.indexOf(dayFrmt) !== -1) {
-              let dateHm = format(dt, 'HH:mm')
               if (midNightRegex.test(dateHm)) {
+                nextDay = format(sub_days(dt, 1), 'YYYY-MM-DD')
               } else {
                 return false
               }
             }
             days.push(dayFrmt)
+            if (nextDay === dayFrmt && twentyThreeHoursRegex.test(dateHm)) {
+              return false
+            }
             return true
           })
         })
