9e8c5c4875c13fda87d71244e35a0641e6a49d25
Tobias Kronthaler <kronthto@gmail.com>
2022-01-15 17:33:41 +0100

    Filter Heatmap by gear UI

--- src/containers/ChromeRivals/MapStats.js
+++ src/containers/ChromeRivals/MapStats.js
@@ -10,7 +10,9 @@ import { parse } from 'querystring'
 class MapStats extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      gear: null
+    }
   }
 
   componentDidMount() {
@@ -23,7 +25,10 @@ class MapStats extends Component {
     }
 
     let heatmapImagePromise = callApiChecked(
-      config.crapibase + 'heatmap/mapkills' + window.location.search
+      config.crapibase +
+        'heatmap/mapkills' +
+        window.location.search +
+        (this.state.gear ? '&gear=' + this.state.gear : '')
     )
 
     this.setState({ heatmapImagePromise, heatmapRes: null })
@@ -71,6 +76,24 @@ class MapStats extends Component {
           <dt>To</dt>
           <dd>{query.to}</dd>
         </dl>
+
+        <div style={{ marginBottom: '10px' }}>
+          Only{' '}
+          {['A', 'B', 'I', 'M'].map(gear => (
+            <span
+              style={{
+                marginRight: '5px',
+                fontWeight: 'bold',
+                cursor: 'pointer'
+              }}
+              onClick={() => {
+                this.setState({ gear }, () => this.queryData())
+              }}
+            >
+              {gear}G
+            </span>
+          ))}
+        </div>
 
         <img
           src={heatmapRes.heatmap}
